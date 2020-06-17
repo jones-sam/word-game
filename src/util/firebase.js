d1856a3b2bed8e7e0e6c28d8f82f626efbd91519
jones-sam <samjones6058@gmail.com>
2020-06-17 18:40:38 -0300

    firebase client settings

---
+++ src/util/firebase.js
@@ -0,0 +1,9 @@
+import * as firebase from "firebase"
+
+import firebaseConfig from "./firebaseConfig"
+
+export const myFirebase = firebase.initializeApp(firebaseConfig)
+myFirebase.analytics()
+
+const baseDb = myFirebase.firestore()
+export const db = baseDb
