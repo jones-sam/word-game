d63331be884d922a785e9502f73649b65691d7a4
jones-sam <samjones6058@gmail.com>
2020-06-17 17:59:27 -0300

    basic home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import home from "./components/home"
 import "./App.css"
 
 function App() {
-  return <h1>test</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={home} />
+        {/* <Route exact path="/lobbies/:code" component={lobby} /> */}
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
+
+function Home() {
+  const [openJoin, setOpenJoin] = useState(false)
+  const [openCreate, setOpenCreate] = useState(false)
+
+  const handleJoinSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted")
+  }
+
+  const handleCreateSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted")
+  }
+
+  return (
+    <>
+      <Grid container justify="center" alignItems="center">
+        <h1>Longest Word Game</h1>
+      </Grid>
+      <Grid
+        container
+        justify="center"
+        direction="column"
+        alignItems="center"
+        style={{ marginTop: "30vh" }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => setOpenJoin(true)}
+        >
+          Join Game
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          onClick={() => setOpenCreate(true)}
+        >
+          Create Game
+        </Button>
+      </Grid>
+      {/* Join lobby dialog */}
+      <Dialog open={openJoin} onClose={() => setOpenJoin(false)} fullWidth>
+        <DialogTitle>Join Game</DialogTitle>
+        <form onSubmit={handleJoinSubmit}>
+          <DialogContent>
+            <DialogContentText>Connect with your friend!</DialogContentText>
+            <TextField
+              autoFocus
+              id="name"
+              label="Name"
+              type="text"
+              fullWidth
+              required
+            />
+            <TextField
+              id="code"
+              label="Access Code"
+              type="text"
+              fullWidth
+              required
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenJoin(false)} color="secondary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary" variant="contained">
+              Submit
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+      {/* New lobby dialog */}
+      <Dialog open={openCreate} onClose={() => setOpenCreate(false)} fullWidth>
+        <DialogTitle>Create Game</DialogTitle>
+        <form onSubmit={handleCreateSubmit}>
+          <DialogContent>
+            <DialogContentText>Create a game</DialogContentText>
+            <TextField
+              autoFocus
+              id="name"
+              label="Name"
+              type="text"
+              fullWidth
+              required
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenCreate(false)} color="secondary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary" variant="contained">
+              Submit
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </>
+  )
+}
+
+export default Home
